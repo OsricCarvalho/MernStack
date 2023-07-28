f68903ff1b0955d402806b1b61240f99295cb444
unknown <you@example.com>
2023-07-28 11:42:09 +0100

    Added Project

---
+++ FrontEnd/project2/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+
+import './App.css';
+
+import ShowAllEmployees from './ShowAllEmployees';
+
+import NewEmployee from './NewEmployee';
+
+function App() {
+
+  return (
+
+    <div>
+
+      <h1> hello </h1>
+
+      <NewEmployee/>
+
+      <ShowAllEmployees/>
+
+    </div>
+
+  );
+
+}
+
+ 
+
+export default App;
